1c383acc2c523c8023e4b0b560e4a84a9129950d
Nino <nino.barbier@edu.devinci.fr>
2021-10-22 12:58:45 +0200

    Ending

---
+++ Assets/Other Assets/fin.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fin : MonoBehaviour
+{
+
+    public GameObject end;
+  
+
+    void Start()
+    {
+        end.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player"))
+        {
+            end.SetActive(true);
+        }
+
+    }
+}
